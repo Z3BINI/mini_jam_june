65a29638170a81540b01e299675a33ce634cdf70
Eusébio Alves <eusebio.a95@hotmail.com>
2024-06-03 11:17:12 +0100

    Horizontal movement logic

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -2,10 +2,18 @@ class_name Player extends Entity
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta : float) -> void:
+	
+	var input_direction : float = Input.get_axis("move_left", "move_right")
+	
+	if input_direction:
+		move(input_direction, delta)
+	else:
+		if velocity.x != 0: 
+			stop("normal", delta)
+		
+	move_and_slide()
