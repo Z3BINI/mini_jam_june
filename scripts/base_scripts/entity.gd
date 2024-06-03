d7f4ee6a57586785e0f749d3c2f0686915ac0bcb
Eusébio Alves <eusebio.a95@hotmail.com>
2024-06-03 09:50:17 +0100

    Entity movement logic
    Using can_move bool for future knock back and other stop logic implementation.

--- scripts/base_scripts/entity.gd
+++ scripts/base_scripts/entity.gd
@@ -1,11 +1,28 @@
 class_name Entity extends CharacterBody2D
 
+@export var MAX_SPEED : float
+@export var ACCEL : float
+@export var DECEL : float
+
+var can_move : bool = true
+var current_speed : float
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta : float) -> void:
 	pass
+	
+func _physics_process(_delta : float) -> void:
+	move_and_slide()
+
+func move(direction : Vector2) -> void:
+	if !can_move: return
+	
+	if !(direction.is_normalized()):
+		direction = direction.normalized()
+		
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCEL)
