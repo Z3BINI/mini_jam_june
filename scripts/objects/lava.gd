6dd580d7a993952e7c344f0fa4484cc3cc091e3c
Eusébio Alves <eusebio.a95@hotmail.com>
2024-06-08 14:39:21 +0100

    Lava size pre-configured
    Was having problems to include all components within the lava due to the resizing. Lava know has a little over window width and height of a size to compensate for the upper part animation and to prevent bottom from being seen.

--- scripts/objects/lava.gd
+++ scripts/objects/lava.gd
@@ -8,8 +8,6 @@ var player : Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	scale = get_viewport_rect().size
-	
 	player = get_tree().get_first_node_in_group("player")
 
 func _physics_process(delta):
