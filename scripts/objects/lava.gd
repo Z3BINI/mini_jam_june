0f249a28abcc260878ca168d491fb827cd3247c5
Eusébio Alves <eusebio.a95@hotmail.com>
2024-06-08 11:13:09 +0100

    Game over condition
    Follows the player on the X axis to simulate infinite horizontal space. Moves upward at a defined speed and a big enough size to freeze time when player dies to avoid showing the bottom end.

---
+++ scripts/objects/lava.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+signal player_died
+
+@export var RISE_SPEED : float = 25
+
+var player : Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	scale = get_viewport_rect().size
+	
+	player = get_tree().get_first_node_in_group("player")
+
+func _physics_process(delta):
+	if player:
+		global_position.x = player.global_position.x
+	
+	global_position.y -= RISE_SPEED * delta
+
+func _on_body_entered(body : Player):
+	player_died.emit()
+	print("Game Over!")
