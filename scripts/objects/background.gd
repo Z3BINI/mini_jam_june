9cdc779540fa06387e38e89189d0a6a7ddbbf51c
Eusébio Alves <eusebio.a95@hotmail.com>
2024-06-11 08:51:05 +0100

    Background split to follow player around

---
+++ scripts/objects/background.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var player : Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	global_position = player.global_position
