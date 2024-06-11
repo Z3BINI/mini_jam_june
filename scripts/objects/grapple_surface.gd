8abfbf4c1c34da0c1de2bc4d80a746976b2bf9a1
Eusébio Alves <eusebio.a95@hotmail.com>
2024-06-11 08:15:35 +0100

    Slow time effect when falling near surface

--- scripts/objects/grapple_surface.gd
+++ scripts/objects/grapple_surface.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 var lava : Area2D
+var player : Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,3 +12,16 @@ func _process(_delta):
 	
 	if global_position.y > lava.global_position.y + 55:
 		queue_free()
+	
+	if player and (player.left_hooked or player.right_hooked):
+		Engine.time_scale = 1
+
+func _on_slow_time_check_body_entered(body : Player):
+	if body.linear_velocity.y >= 0:
+		Engine.time_scale = 0.15
+		player = body
+
+func _on_slow_time_check_body_exited(body : Player):
+	if Engine.time_scale < 1:
+		Engine.time_scale = 1
+		player = null
